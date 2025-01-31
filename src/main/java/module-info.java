8a8f8ab9cc7fb498520f783437d25119c77e7c67
Min Pyae Hein <minpyaehein536h@prestigein.com>
2025-01-31 20:21:54 +0700

    change code

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,11 @@ module org.example.mylearningproject {
     requires javafx.fxml;
     requires org.kordamp.bootstrapfx.core;
     requires java.sql;
+
     opens org.example.mylearningproject to javafx.fxml;
     opens org.example.mylearningproject.view to javafx.fxml;
     opens Model to javafx.base;
+
     exports Model;
     exports Controller;
     opens Controller to javafx.fxml;
